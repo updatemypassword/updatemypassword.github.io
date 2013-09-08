d673ec0de66cf7e70d64b653397f11eb9274daa9
Dries De Peuter <dries@nousefreak.be>
2013-09-08 14:59:41 +0200

    Move view and add namespace to controller

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ angular.module('ump', [])
     .value('dsSites', 'sites.json')
     .config(function($routeProvider) {
         $routeProvider.
-            when('/', {controller:UmpListCtrl, templateUrl:'_list.html'})
+            when('/', {controller: 'List', templateUrl: 'views/list.html'})
     });
 
-function UmpListCtrl($scope, $http, dsSites) {
-    $http.get(dsSites).success(function(data) {
-        $scope.sites = data;
+angular.module('ump')
+    .controller('List', function($scope, $http, dsSites) {
+        $http.get(dsSites).success(function(data) {
+            $scope.sites = data;
+        });
+        $scope.orderProp = 'name';
     });
-    $scope.orderProp = 'name';
-}
